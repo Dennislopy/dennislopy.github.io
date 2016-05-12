57a616649dd67ab6db2273cb27b873fa207bb7cf
Digital Media Student <digital@browardtech.local>
2016-05-12 13:52:38 -0400

    webfinal

---
+++ WebBanner/webfinal/final animated web banner 300x600_edge.js
@@ -0,0 +1,70 @@
+
+(function(compId){var _=null,y=true,n=false,x3='6.0.0',x4='5.0.0',x6='rgba(0,0,0,0)',g='image',x40='87px',e29='${RAM}',x47='39px',x48='Ram',e28='${Graphics_card}',x45='Power_Supply',x44='75px',m='rect',e22='${Click_The_Parts}',x31='CPU',e20='${gaming_rig2}',i='none',x35='auto',x5='6.0.0.400',x37='78px',e21='${Motherboard}',x36='103px',e27='${Power}',e30='${CPU3}',x41='54px',x34='64px',x33='98px',x32='0px',x38='Grpahics_Card',e26='${Build_button-1}',lf='left',x9='rgba(0,173,255,1.00)',x42='Motherboard',tp='top',e23='${google_play_button}',e24='${Downlaod_the_APP_NOW_}',x19='rgba(255,255,255,1)',e25='${apple-logo-white-md}',o='opacity';var g13='Build%20button-1.png',g49='Ram.png',g18='269295__kwahmah-02__swoosh38.wav',g39='Grpahics%20Card.png',g16='google%20play%20button.png',g7='blue%20blur.jpg',g2='jquery-ui.min.js',g43='Motherboard.png',g11='CPU.png',g14='Ryg%20Builder%20Logo%20white.png',g10='phone.png',g46='Power%20Supply.png',g12='Click%20The%20Parts.png',g17='apple-logo-white-md.png',g15='gaming%20rig.png',g1='jquery-ui.js',g8='Downlaod%20the%20APP%20NOW_.png';var im='images/',aud='media/',vid='media/',js='js/',fonts={},opts={'gAudioPreloadPreference':'auto','gVideoPreloadPreference':'auto'},resources=[],scripts=[js+g1,js+g2],symbols={"stage":{v:x3,mv:x4,b:x5,stf:i,cg:i,rI:n,cn:{dom:[{id:'blue_blur',t:g,r:['-316px','2px','1105px','776px','auto','auto'],cl:'rect(-28px 617px 600.33349609375px 312px)',f:[x6,im+g7,'0px','0px'],tf:[[],[],[],['0.98689','1.00424']]},{id:'Downlaod_the_APP_NOW_',t:g,r:['42px','79px','228px','101px','auto','auto'],o:'0',f:[x6,im+g8,'0px','0px']},{id:'Rectangle2',t:m,r:['104px','409px','92px','163px','auto','auto'],f:[x9],s:[3,"rgba(54,54,54,1.00)",i]},{id:'phone',t:g,r:['98px','385px','104px','209px','auto','auto'],f:[x6,im+g10,'0px','0px']},{id:'Graphics_card',symbolName:'Graphics_card',t:m,r:['156px','79px','103','78','auto','auto'],cu:'pointer',o:'0'},{id:'CPU3',t:g,r:['46px','86px','98px','63px','auto','auto'],cu:'pointer',o:'0',f:[x6,im+g11,'0px','0px']},{id:'Motherboard',symbolName:'Motherboard',t:m,r:['12px','183px','87','54','auto','auto'],cu:'pointer',o:'0'},{id:'Power',symbolName:'Power',t:m,r:['112px','178px','75','64','auto','auto'],overflow:'hidden',cu:'pointer',o:'0'},{id:'RAM',symbolName:'RAM',t:m,r:['202px','190px','87','39','auto','auto'],cu:'pointer',o:'0'},{id:'Click_The_Parts',t:g,r:['24px','-59px','254px','39px','auto','auto'],o:'0',f:[x6,im+g12,'0px','0px']},{id:'Build_button-1',t:g,r:['295px','182px','277px','277px','auto','auto'],cl:'rect(97.333251953125px 239.6669921875px 174.33349609375px 40.00048828125px)',cu:'pointer',o:'0.000000',f:[x6,im+g13,'0px','0px']},{id:'Ryg_Builder_Logo_white',t:g,r:['17px','11px','109px','31px','auto','auto'],f:[x6,im+g14,'0px','0px']},{id:'RAM-1',symbolName:'RAM',t:m,r:['107px','480px','87','39','auto','auto'],o:'0'},{id:'Power-1',symbolName:'Power',t:m,r:['113px','464px','75','64','auto','auto'],o:'0'},{id:'Motherboard-1',symbolName:'Motherboard',t:m,r:['106px','469px','87','54','auto','auto'],o:'0'},{id:'Graphics_card-1',symbolName:'Graphics_card',t:m,r:['99px','457px','103','78','auto','auto'],o:'0'},{id:'CPU3-1',t:g,r:['98px','464px','98px','63px','auto','auto'],o:'0',f:[x6,im+g11,'0px','0px']},{id:'gaming_rig2',t:g,r:['-13px','550px','328px','326px','auto','auto'],overflow:'hidden',cl:'rect(0px 328.2666015625px 327.06689453125px 0px)',o:'0',f:[x6,im+g15,'0px','0px'],tf:[[],[],[],['0.69425','0.69425']]},{id:'google_play_button',t:g,r:['-159px','174px','215px','215px','auto','auto'],cl:'rect(0px 204px 215px 0px)',cu:'pointer',o:'0',f:[x6,im+g16,'0px','0px'],tf:[[],[],[],['0.48372','0.48372']]},{id:'apple-logo-white-md',t:g,r:['333px','229px','75px','92px','auto','auto'],cu:'pointer',o:'0',f:[x6,im+g17,'0px','0px']},{id:'_269295__kwahmah-02__swoosh38',v:i,t:'audio',tag:'audio',r:['495','338','320px','45px','auto','auto'],sr:[aud+g18],pr:'auto'}],style:{'${Stage}':{isStage:true,r:['null','null','300px','600px','auto','auto'],overflow:'visible',f:[x19]}}},tt:{d:4000,a:y,l:{"Stop":2500,"Start":3000},data:[["eid84",tp,3000,1000,"easeOutCubic",e20,'550px','324px'],["eid55",o,1000,500,"easeOutCubic",e21,'0','1'],["eid140",o,3000,0,"easeOutCubic",e21,'1','0'],["eid164",tp,0,1000,"easeOutQuart",e22,'-59px','313px'],["eid153",o,3000,1000,"easeOutCubic",e23,'0.000000','1'],["eid145",o,3000,1000,"easeOutCubic",e24,'0.000000','1'],["eid149",o,3000,1000,"easeOutCubic",e25,'0','1'],["eid91",o,0,0,"easeOutCubic",e26,'0.000000','0.000000'],["eid166",o,0,1000,"easeOutSine",e22,'0','1'],["eid179",o,3005,0,"easeOutSine",e22,'1','0'],["eid151",lf,3000,1000,"easeOutSine",e25,'333px','184px'],["eid160",lf,2040,460,"easeOutCubic",e26,'295px','11px'],["eid182",lf,3000,1000,"easeOutCubic",e26,'11px','278px'],["eid57",o,1500,500,"easeOutCubic",e27,'0.000000','1'],["eid129",o,3000,0,"easeOutCubic",e27,'0','0'],["eid100",o,3000,1000,"easeOutCubic",e20,'0','1'],["eid53",o,500,500,"easeOutCubic",e28,'0','1'],["eid142",o,3000,0,"easeOutCubic",e28,'1','0'],["eid59",o,2000,500,"easeOutCubic",e29,'0','1'],["eid117",o,3000,0,"easeOutCubic",e29,'0','0'],["eid51",o,0,500,"easeOutCubic",e30,'0.000000','1'],["eid136",o,3000,0,"easeOutCubic",e30,'0.000000','0'],["eid155",lf,3000,1000,"easeOutSine",e23,'-159px','-9px'],["eid161","tr",3000,function(e,d){this.eMA(e,d);},['play','${_269295__kwahmah-02__swoosh38}',[]]],["eid162","tr",4000,function(e,d){this.eMA(e,d);},['pause','${_269295__kwahmah-02__swoosh38}',[]]]]}},"CPU":{v:x3,mv:x4,b:x5,stf:i,cg:i,rI:n,cn:{dom:[{id:x31,t:g,r:[x32,x32,x33,x34,x35,x35],f:[x6,im+g11,x32,x32]}],style:{'${symbolSelector}':{r:[_,_,x33,x34]}}},tt:{d:0,a:y,data:[]}},"Graphics_card":{v:x3,mv:x4,b:x5,stf:i,cg:i,rI:n,cn:{dom:[{r:[x32,x32,x36,x37,x35,x35],id:x38,t:g,f:[x6,im+g39,x32,x32]}],style:{'${symbolSelector}':{r:[_,_,x36,x37]}}},tt:{d:0,a:y,data:[]}},"Motherboard":{v:x3,mv:x4,b:x5,stf:i,cg:i,rI:n,cn:{dom:[{r:[x32,x32,x40,x41,x35,x35],id:x42,t:g,f:[x6,im+g43,x32,x32]}],style:{'${symbolSelector}':{r:[_,_,x40,x41]}}},tt:{d:0,a:y,data:[]}},"Power":{v:x3,mv:x4,b:x5,stf:i,cg:i,rI:n,cn:{dom:[{r:[x32,x32,x44,x34,x35,x35],id:x45,t:g,f:[x6,im+g46,x32,x32]}],style:{'${symbolSelector}':{r:[_,_,x44,x34]}}},tt:{d:0,a:y,data:[]}},"RAM":{v:x3,mv:x4,b:x5,stf:i,cg:i,rI:n,cn:{dom:[{r:[x32,x32,x40,x47,x35,x35],id:x48,t:g,f:[x6,im+g49,x32,x32]}],style:{'${symbolSelector}':{r:[_,_,x40,x47]}}},tt:{d:0,a:y,data:[]}}};AdobeEdge.registerCompositionDefn(compId,symbols,fonts,scripts,resources,opts);})("EDGE-265053345");
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;Edge.registerEventBinding(compId,function($){
+//Edge symbol: 'stage'
+(function(symbolName){Symbol.bindElementAction(compId,symbolName,"${Stage}","click",function(sym,e){});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${CPU3}","click",function(sym,e){sym.$("CPU3").css({opacity:0,transition:"all 0.25s ease-in"});sym.$("Motherboard-1").css({opacity:0,transition:"all 0.25s ease-in"});sym.$("Graphics_card-1").css({opacity:0,transition:"all 0.25s ease-in"});sym.$("Power-1").css({opacity:0,transition:"all 0.25s ease-in"});sym.$("RAM-1").css({opacity:0,transition:"all 0.25s ease-in"});sym.$("CPU3-1").css({opacity:1,transition:"all 0.25s ease-in"});window.nocp-=1
+if(window.nocp===0)window.finishRygAnimation()});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${Graphics_card}","click",function(sym,e){sym.$("Graphics_card").css({opacity:0,transition:"all 0.25s ease-in"});sym.$("Graphics_card-1").css({opacity:1,transition:"all 0.25s ease-in"});sym.$("CPU3-1").css({opacity:0,transition:"all 0.25s ease-in"});sym.$("Motherboard-1").css({opacity:0,transition:"all 0.25s ease-in"});sym.$("Power-1").css({opacity:0,transition:"all 0.25s ease-in"});sym.$("RAM-1").css({opacity:0,transition:"all 0.25s ease-in"});window.nocp-=1
+if(window.nocp===0)window.finishRygAnimation()});
+//Edge binding end
+Symbol.bindSymbolAction(compId,symbolName,"creationComplete",function(sym,e){});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"document","compositionReady",function(sym,e){window.nocp=5
+window.finishRygAnimation=function(){console.log('wewlad');sym.$("Build_button-1").css({opacity:1,transition:'all 1s ease-in'})
+sym.$("Click_The_Parts").css({opacity:0,transition:'all 0.25s ease-out'})};});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${CPU3-1}","click",function(sym,e){});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${Motherboard}","click",function(sym,e){sym.$("Motherboard").css({opacity:0,transition:"all 0.25s ease-in"});sym.$("Motherboard-1").css({opacity:1,transition:"all 0.25s ease-in"});sym.$("Graphics_card-1").css({opacity:0,transition:"all 0.25s ease-in"});sym.$("CPU3-1").css({opacity:0,transition:"all 0.25s ease-in"});sym.$("Power-1").css({opacity:0,transition:"all 0.25s ease-in"});sym.$("RAM-1").css({opacity:0,transition:"all 0.25s ease-in"});window.nocp-=1
+if(window.nocp===0)window.finishRygAnimation()});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${Power}","click",function(sym,e){sym.$("Power").css({opacity:0,transition:"all 0.25s ease-in"});sym.$("Power-1").css({opacity:1,transition:"all 0.25s ease-in"});sym.$("Motherboard-1").css({opacity:0,transition:"all 0.25s ease-in"});sym.$("Graphics_card-1").css({opacity:0,transition:"all 0.25s ease-in"});sym.$("CPU3-1").css({opacity:0,transition:"all 0.25s ease-in"});sym.$("RAM-1").css({opacity:0,transition:"all 0.25s ease-in"});window.nocp-=1
+console.log(window.nocp);if(window.nocp===0)window.finishRygAnimation()});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${RAM}","click",function(sym,e){sym.$("RAM").css({opacity:0,transition:"all 0.25s ease-in"});sym.$("RAM-1").css({opacity:1,transition:"all 0.25s ease-in"});sym.$("Motherboard-1").css({opacity:0,transition:"all 0.25s ease-in"});sym.$("Graphics_card-1").css({opacity:0,transition:"all 0.25s ease-in"});sym.$("Power-1").css({opacity:0,transition:"all 0.25s ease-in"});sym.$("CPU3-1").css({opacity:0,transition:"all 0.25s ease-in"});window.nocp-=1
+if(window.nocp===0)window.finishRygAnimation()});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${Build_button-1}","click",function(sym,e){sym.play("Start");});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",2960,function(sym,e){sym.stop("Stop");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${google_play_button}","click",function(sym,e){window.open("https://play.google.com/store");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${apple-logo-white-md}","click",function(sym,e){window.open("http://www.apple.com/itunes/");});
+//Edge binding end
+})("stage");
+//Edge symbol end:'stage'
+
+//=========================================================
+
+//Edge symbol: 'CPU'
+(function(symbolName){})("CPU");
+//Edge symbol end:'CPU'
+
+//=========================================================
+
+//Edge symbol: 'Graphics_card'
+(function(symbolName){})("Graphics_card");
+//Edge symbol end:'Graphics_card'
+
+//=========================================================
+
+//Edge symbol: 'Motherboard'
+(function(symbolName){})("Motherboard");
+//Edge symbol end:'Motherboard'
+
+//=========================================================
+
+//Edge symbol: 'Power'
+(function(symbolName){})("Power");
+//Edge symbol end:'Power'
+
+//=========================================================
+
+//Edge symbol: 'RAM'
+(function(symbolName){})("RAM");
+//Edge symbol end:'RAM'
+})})(AdobeEdge.$,AdobeEdge,"EDGE-265053345");
